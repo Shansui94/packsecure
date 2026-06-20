2247d01b5479e712b751f3683cf1ce1d679d6345
Max Tan <khailoon94@gmail.com>
2026-06-20 15:41:27 +0800

    fix(dom): apply Node removeChild polyfill and block page translation to prevent DOM reconciliation crashes

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,29 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
+// React DOM reconciliation crash shield (Google Translate / DOM modification bugfix)
+if (typeof window !== 'undefined' && typeof Node !== 'undefined' && Node.prototype) {
+    const originalRemoveChild = Node.prototype.removeChild;
+    Node.prototype.removeChild = function <T extends Node>(child: T): T {
+        if (child && child.parentNode !== this) {
+            console.warn('Node.removeChild: child is not a child of this node', child, this);
+            if (child.parentNode) {
+                return originalRemoveChild.call(child.parentNode, child) as T;
+            }
+            return child;
+        }
+        return originalRemoveChild.apply(this, arguments as any) as T;
+    };
+
+    const originalInsertBefore = Node.prototype.insertBefore;
+    Node.prototype.insertBefore = function <T extends Node>(newNode: T, referenceNode: Node | null): T {
+        if (referenceNode && referenceNode.parentNode !== this) {
+            console.warn('Node.insertBefore: referenceNode is not a child of this node', referenceNode, this);
+            return originalInsertBefore.call(referenceNode.parentNode || this, newNode, referenceNode) as T;
+        }
+        return originalInsertBefore.apply(this, arguments as any) as T;
+    };
+}
 import './index.css';
 import App from './App';
 import ErrorBoundary from './components/ErrorBoundary';
